696e5d7b8c262f000658eb1ecb26396f91493b2a
Adi-K-Coding <34950822+Adi-K-Coding@users.noreply.github.com>
2021-05-09 16:31:26 -0700

    factorial, stringTrimmer, RecursionMath+Test

--- src/_02_RecursionMath/RecursionMath.java
+++ src/_02_RecursionMath/RecursionMath.java
@@ -2,26 +2,38 @@ package _02_RecursionMath;
 
 public class RecursionMath {
 
-    public static int recursiveMultiplication(int number, int times) {
-        // If times is 1 
-            // Return number 
+	public static int recursiveMultiplication(int number, int times) {
+		// If times is 1
+		if (times == 1) {
+			return number;
+		} else if ((times == 0) || (number == 0)) {
+			return 0;
+		}
+		// Return number
 
-        // Else return number + recursionMultiplication(number, times-1)
+		// Else return number + recursionMultiplication(number, times-1)
+		return number + recursiveMultiplication(number, times - 1);
 
-        return 0;
-    }
+	}
 
-    // Try this one on your own! 
-    // Hint: if numberToDivideBy is bigger than number,
-    //       you can't divide anymore
-    public static int recursiveDivision(int number, int numberToDevideBy) {
-        
-        return 0;
-    }
+	// Try this one on your own!
+	// Hint: if numberToDivideBy is bigger than number,
+	// you can't divide anymore
+	public static int recursiveDivision(int number, int numberToDivideBy) {
+		if (numberToDivideBy == 0) {
+			return 0;
+		} else if (numberToDivideBy > number) {
+			return 0;
+		}
+		return 1 + recursiveDivision(number - numberToDivideBy, numberToDivideBy);
 
-    // Try this one on your own!
-    public static int recursivePower(int number, int power) {
-        
-        return 0;
-    }
+	}
+
+	// Try this one on your own!
+	public static int recursivePower(int number, int power) {
+		if (power == 0) {
+			return 1;
+		}
+		return number * recursivePower(number, power - 1);
+	}
 }
--- src/_02_RecursionMath/RecursionMathTest.java
+++ src/_02_RecursionMath/RecursionMathTest.java
@@ -6,24 +6,43 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class RecursionMathTest {
-    
-    @Test
-    public void testMultiplication() {
-        assertEquals(12, RecursionMath.recursiveMultiplication(3, 4));
-        
-        // Add more JUnit tests like the one above to test your method
-        
-    }
 
-    @Test
-    public void testDivision() {
-        // Add JUnit tests to test your method
-        fail("Write some tests!");
-    }
+	@Test
+	public void testMultiplication() {
+		assertEquals(12, RecursionMath.recursiveMultiplication(3, 4));
+		assertEquals(20, RecursionMath.recursiveMultiplication(2, 10));
+		assertEquals(20, RecursionMath.recursiveMultiplication(10, 2));
+		assertEquals(1, RecursionMath.recursiveMultiplication(1, 1));
+		assertEquals(20, RecursionMath.recursiveMultiplication(5, 4));
+		assertEquals(0, RecursionMath.recursiveMultiplication(0, 8));
+		assertEquals(0, RecursionMath.recursiveMultiplication(8, 0));
+		// Add more JUnit tests like the one above to test your method
 
-    @Test 
-    public void testPower() {
-        // Add JUnit tests to test your method
-        fail("Write some tests!");
-    }
+	}
+
+	@Test
+	public void testDivision() {
+		// Add JUnit tests to test your method
+		assertEquals(5, RecursionMath.recursiveDivision(20, 4));
+		assertEquals(0, RecursionMath.recursiveDivision(20, 0));
+		assertEquals(5, RecursionMath.recursiveDivision(5, 1));
+		assertEquals(1, RecursionMath.recursiveDivision(1, 1));
+		assertEquals(12, RecursionMath.recursiveDivision(48, 4));
+		assertEquals(13, RecursionMath.recursiveDivision(26, 2));
+		assertEquals(4, RecursionMath.recursiveDivision(1000, 250));
+	}
+
+	@Test
+	public void testPower() {
+		// Add JUnit tests to test your method
+		assertEquals(1, RecursionMath.recursivePower(1, 1));
+		assertEquals(1, RecursionMath.recursivePower(1, 0));
+		assertEquals(0, RecursionMath.recursivePower(0, 1));
+		assertEquals(1, RecursionMath.recursivePower(1, 1));
+		assertEquals(5, RecursionMath.recursivePower(5, 1));
+		assertEquals(64, RecursionMath.recursivePower(4, 3));
+		assertEquals(9, RecursionMath.recursivePower(3, 2));
+		assertEquals(729, RecursionMath.recursivePower(9, 3));
+		assertEquals(4096, RecursionMath.recursivePower(8, 4));
+	}
 }
